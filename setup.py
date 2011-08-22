e676aec344dca7899da5617b73fd6d76ae781b99
Kenneth Reitz <me@kennethreitz.com>
2011-08-22 15:13:13 -0400

    v0.1.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if sys.version_info[:2] < (2,6):
 
 setup(
     name='readability-api',
-    version='0.1.2',
+    version='0.1.3',
     description='Python wrapper for the Readability API.',
     long_description=open('README.rst').read(),
     author='The Readability Team',
